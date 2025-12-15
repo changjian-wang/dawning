164800b0cfd55534653a71ab4d4474158800a5c4
Changjian Wang <v-changjwang@microsoft.com>
2025-12-15 10:13:25 +0800

    fix: add Column attributes to UserRoleEntity for snake_case mapping

--- Dawning.Gateway/src/Dawning.Identity.Infra.Data/PersistentObjects/Administration/UserRoleEntity.cs
+++ Dawning.Gateway/src/Dawning.Identity.Infra.Data/PersistentObjects/Administration/UserRoleEntity.cs
@@ -13,26 +13,31 @@ namespace Dawning.Identity.Infra.Data.PersistentObjects.Administration
         /// 关联ID
         /// </summary>
         [ExplicitKey]
+        [Column("id")]
         public Guid Id { get; set; }
 
         /// <summary>
         /// 用户ID
         /// </summary>
+        [Column("user_id")]
         public Guid UserId { get; set; }
 
         /// <summary>
         /// 角色ID
         /// </summary>
+        [Column("role_id")]
         public Guid RoleId { get; set; }
 
         /// <summary>
         /// 分配时间
         /// </summary>
+        [Column("created_at")]
         public DateTime CreatedAt { get; set; }
 
         /// <summary>
         /// 分配者ID
         /// </summary>
+        [Column("created_by")]
         public Guid? CreatedBy { get; set; }
     }
 }
